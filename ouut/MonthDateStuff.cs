303920fd69e3469a6c187f2dfed9d481af7d489e
Mark McFadden <m2web@yahoo.com>
2018-12-17 20:34:46 -0500

    Added more tests and cleaned up implementation class

--- ouut/MonthDateStuff.cs
+++ ouut/MonthDateStuff.cs
@@ -4,10 +4,9 @@ namespace ouut
 {
     public class MonthDateStuff
     {
-        public int GetMonthAsInteger()
+        public int GetJanuaryMonthAsInteger()
         {
-            var dateInput = "Jan 1, 2019";
-            var theDate = DateTime.Parse(dateInput);
+            var theDate = DateTime.Parse("Jan 1, 2019");
             
             return theDate.Month;
         }
@@ -18,6 +17,25 @@ namespace ouut
 
         public int GetLastDayOfTheMonth(int year, int month){
             return DateTime.DaysInMonth(year, month);
+        }
+
+        public DateTime GetStartDateOfVerseReviewPeriod(DateTime today){
+            DateTime lastDayOfMonthDate = new DateTime (today.Year, today.Month,
+                                    DateTime.DaysInMonth(today.Year, today.Month));
+            var result = lastDayOfMonthDate.AddDays(-4);
+            return result;
+        }
+
+        public DateTime GetEndDateOfVerseReviewPeriod(DateTime today){
+            DateTime lastDayOfMonthDate = new DateTime (today.Year, today.Month,
+                                    DateTime.DaysInMonth(today.Year, today.Month));
+            return lastDayOfMonthDate;
+        }
+
+        public bool IsDateInVerseReviewPeriod(DateTime today){
+            var startDate = GetStartDateOfVerseReviewPeriod(today);
+            var endDate = GetEndDateOfVerseReviewPeriod(today);
+            return today >= startDate && today <= endDate;
         }
     }
 }
--- tests/MonthDateStuffTests.cs
+++ tests/MonthDateStuffTests.cs
@@ -15,7 +15,7 @@ namespace tests
         [Fact]
         public void ReturnIntegerGivenValidDate()
         {
-            var result = _monthDateStuff.GetMonthAsInteger();
+            var result = _monthDateStuff.GetJanuaryMonthAsInteger();
 
             Assert.True(result.Equals(1));
         }
@@ -38,6 +38,48 @@ namespace tests
             var result = _monthDateStuff.GetLastDayOfTheMonth(year, month);
 
             Assert.True(result.Equals(31));
+        }
+
+        [Fact]
+        public void ReturnReviewStartDateGivenCurrent()
+        {
+            var today = DateTime.Parse("December 2, 2018");
+            var result = _monthDateStuff.GetStartDateOfVerseReviewPeriod(today);
+            var theDate = DateTime.Parse("December 27, 2018");
+            
+            Assert.True(result.Equals(theDate));
+        }
+
+        [Fact ]
+        public void ReturnReviewEndDateGivenCurrent()
+        {
+            var today = DateTime.Parse("December 2, 2018");
+            var result = _monthDateStuff.GetEndDateOfVerseReviewPeriod(today);
+            var theDate = DateTime.Parse("December 31, 2018");
+            
+            Assert.True(result.Equals(theDate));
+        }
+
+        [Theory]
+        [InlineData(1)]
+        [InlineData(10)]
+        [InlineData(15)]
+        public void ReturnIsNotCurrentDateInVerseReviewPeriod(int dayOfMonthInteger){
+            var today = new DateTime (2019, 1, dayOfMonthInteger, 0, 0, 0);
+            var result = _monthDateStuff.IsDateInVerseReviewPeriod(today);
+            
+            Assert.False(result);
+        }
+
+        [Theory]
+        [InlineData(27)]
+        [InlineData(28)]
+        [InlineData(30)]
+        public void ReturnIsCurrentDateInVerseReviewPeriod(int dayOfMonthInteger){
+            var today = new DateTime (2019, 1, dayOfMonthInteger, 0, 0, 0);
+            var result = _monthDateStuff.IsDateInVerseReviewPeriod(today);
+            
+            Assert.True(result);
         }
     }
 }
